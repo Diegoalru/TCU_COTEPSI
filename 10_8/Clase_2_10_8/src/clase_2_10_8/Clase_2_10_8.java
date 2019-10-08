1439860639358af22b2976c6abfd4f8709f3b5c3
diego <diego@kali>
2019-10-08 10:36:53 -0600

    Proyectos agregados.

---
+++ 10_8/Clase_2_10_8/src/clase_2_10_8/Clase_2_10_8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase_2_10_8;
+
+/**
+ *
+ * @author diego
+ */
+public class Clase_2_10_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
