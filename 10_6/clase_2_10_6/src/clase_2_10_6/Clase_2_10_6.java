f9e044259020093379449a8cf62a0d57485ebbf7
diego <diego@kali>
2019-10-08 21:29:00 -0600

    Clase: 2 Grupo: 10-8 Tema: IF (Frames)

---
+++ 10_6/clase_2_10_6/src/clase_2_10_6/Clase_2_10_6.java
@@ -0,0 +1,13 @@
+package clase_2_10_6;
+
+import clase_2_10_6.Frame;
+        
+public class Clase_2_10_6 
+{
+    public static void main(String[] args) 
+    {
+        //Prueba de Frames.
+        Frame frame = new Frame();
+        frame.show();
+    }
+}
